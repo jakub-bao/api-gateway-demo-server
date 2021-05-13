678574e12e8a8355819e668d2009c3bc36a68e5f
Jakub <jflaska@baosystems.com>
2021-05-13 15:16:30 -0400

    basics

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/cookieTest', (req, res) => {
+    res.send(req.headers.cookie);
+})
+
+app.listen(port, () => {
+    console.log(`API Gateway demo app listening at http://localhost:${port}`)
+})
